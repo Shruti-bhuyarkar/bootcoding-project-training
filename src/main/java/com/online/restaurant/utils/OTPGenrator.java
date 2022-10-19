2691053e3b1af43cb32ea260a140862dbd393f4c
internsbpsi <interns@panhealth.com>
2022-10-19 16:19:03 +0530

    code refactor + otp Genrator

--- src/main/java/com/online/restaurant/utils/OTPGenrator.java
+++ src/main/java/com/online/restaurant/utils/OTPGenrator.java
@@ -16,8 +16,8 @@ public class OTPGenrator {
      }
      public static  void main(String[] args) {
          for(int i=0; i<100; i++){
-
+             OTPGenrator.getOTP();
          }
-         OTPGenrator.getOTP();
+
      }
 }
